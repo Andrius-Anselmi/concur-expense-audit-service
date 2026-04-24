5f6208cda28856e7cbf660be9a64c5e050f8bf6a
andrius anselmi <andriusanselmi12@gmail.com>
2026-04-23 22:12:53 -0300

    chore: initial spring boot project structure with maven

---
+++ src/main/java/com/sap/concur/concurexpenseaudit/ConcurExpenseAuditServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sap.concur.concurexpenseaudit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurExpenseAuditServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurExpenseAuditServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sap/concur/concurexpenseaudit/ConcurExpenseAuditServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sap.concur.concurexpenseaudit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurExpenseAuditServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
